3004bee3a9c63359296505de7871aefa57cb6d0e
her1ndra <herindrandhika7@gmail.com>
2026-05-13 01:18:42 +0700

    FIX CONFIG

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2/promise');
 const dbPool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
